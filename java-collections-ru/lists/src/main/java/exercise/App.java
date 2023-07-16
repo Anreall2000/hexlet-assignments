4201c4fb5c789bb9c07d07a28919cab5c945ddea
@hexlet/cli <support@hexlet.io>
2023-07-16 19:25:12 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,9 +1,16 @@
 package exercise;
 
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
-// BEGIN
-
-//END
+class App {
+    public static boolean scrabble(String letters, String word) {
+        var lettersList = new LinkedList<>(letters.toLowerCase().chars().mapToObj(e -> (char) e).toList());
+        for (char letter: word.toLowerCase().toCharArray()) {
+            if (!lettersList.contains(letter)) {
+                return false;
+            }
+            lettersList.remove((Character) letter);
+        }
+        return true;
+    }
+}
